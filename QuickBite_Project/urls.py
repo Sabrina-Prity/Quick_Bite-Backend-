cda2323ad51db9db4253bc7115968d8bc0018146
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-05 15:02:51 +0600

    commited 5

--- QuickBite_Project/urls.py
+++ QuickBite_Project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +29,8 @@ urlpatterns = [
     path('food/', include('food.urls')),
     path('cart/', include('cart.urls')),
     path('category/', include('category.urls')),
-    path('', include('payment.urls'))
+    path('', include('payment.urls')),
+    re_path(r'^favicon\.png$', RedirectView.as_view(url='/static/favicon.png', permanent=True)),
 ]
 
 
