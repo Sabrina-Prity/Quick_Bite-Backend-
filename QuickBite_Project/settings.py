48b42067bf5e34e4e5f29dc5b0e6ea68562a2572
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-05 14:56:32 +0600

    commited 5

--- QuickBite_Project/settings.py
+++ QuickBite_Project/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'crispy_forms',
